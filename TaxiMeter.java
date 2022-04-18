471677022e798acaaebe2954c18511b6b177043e
ferman ayaz <103703791+ayazferman@users.noreply.github.com>
2022-04-18 03:47:36 +0300

    TaxiMeter.java
    Java code for caculete the taxi meter

---
+++ TaxiMeter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaxiMeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		double perKm=2.20, total;
+		int km, startPrice=10;
+		System.out.println("Mesafeyi KM cinsinden giriniz: ");
+		km=scan.nextInt();
+		total=(km*perKm)+startPrice;
+		total=total< 20 ? 20 : total;
+		System.out.println("Total price amount= " + total);
+		
+
+	}
+}
