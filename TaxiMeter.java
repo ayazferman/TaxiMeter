9467fd3ae68428c4ca407bfb6039bef9fc028da5
ferman ayaz <103703791+ayazferman@users.noreply.github.com>
2022-04-18 03:49:37 +0300

    Update TaxiMeter.java

--- TaxiMeter.java
+++ TaxiMeter.java
@@ -7,7 +7,7 @@ public class TaxiMeter {
 		Scanner scan = new Scanner(System.in);
 		double perKm=2.20, total;
 		int km, startPrice=10;
-		System.out.println("Mesafeyi KM cinsinden giriniz: ");
+		System.out.println("Enter the distance in KM: ");
 		km=scan.nextInt();
 		total=(km*perKm)+startPrice;
 		total=total< 20 ? 20 : total;
